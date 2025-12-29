34687d471f7cdd062065425528c58250dd73eb89
Haquenawaz Khan <haquenawazkhan007@gmail.com>
2025-12-29 23:41:36 +0530

    2nd Commit of the day

--- App.js
+++ App.js
@@ -1,4 +1,6 @@
-const parent = React.createElement('div',{id: 'parent'},React.createElement('div',{id: 'child'}, [React.createElement('h1',{},'I am an h1 tag'), React.createElement('h2',{},'I am an h2 tag')]));
+import React from "react";
+import ReactDOM from "react-dom/client";
+const parent = React.createElement('div',{id: 'parent'},React.createElement('div',{id: 'child'}, [React.createElement('h1',{},'This is Namaste React🚀'), React.createElement('h2',{},'I am an h2 tag')]));
 
 const heading = React.createElement("h1", {id: 'heading'}, "Hello World from React!");
 
