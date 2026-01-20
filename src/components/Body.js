aa7ffeb22c7f0b1813052d9c16c74e5a509d95f7
Haquenawaz Khan <haquenawazkhan007@gmail.com>
2026-01-20 19:49:26 +0530

    another try for commit today.

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,11 +1,22 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 
 const Body = () => {
   
   //Local State Variable- Super Powerful variable
   const [listOfRestaurants, setListofRestaurant] = useState(resList);
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const fetchData = async () => {
+    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+
+    const json = await data.json();
+    console.log(json);
+  };
 
   return (
     <div className="body">
