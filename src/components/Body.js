811d499f6fefb74eec2f9d5d209009beb6058c05
Haquenawaz Khan <haquenawazkhan007@gmail.com>
2026-01-21 20:48:33 +0530

    21-01-2026 commit

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,22 +1,27 @@
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
 import {useState, useEffect} from 'react';
+import Shimmer from "./Shimmer";
 
 const Body = () => {
   
   //Local State Variable- Super Powerful variable
-  const [listOfRestaurants, setListofRestaurant] = useState(resList);
+  const [listOfRestaurants, setListofRestaurant] = useState([]);
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch('https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING');
+    const data = await fetch('https://www.swiggy.com/mapi/restaurants/list/v5?offset=0&is-seo-homepage-enabled=true&lat=28.6219055&lng=77.3683319&carousel=true&third_party_vendor=1&screenType=DESKTOP_WEB_LISTING');
 
     const json = await data.json();
     console.log(json);
+    setListofRestaurant(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
   };
+
+  if(listOfRestaurants.length === 0){
+    return <Shimmer />;
+  }
 
   return (
     <div className="body">
